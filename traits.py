2f2aba5b65d60dd3d69015482adfbf0389f71a42
Jeremy <jeremy.savage@hotmai.co.uk>
2021-10-23 20:05:30 +0100

    Added more ape information in the csv files for later processing.

--- traits.py
+++ traits.py
@@ -1,6 +1,8 @@
 
+from numpy import NaN
 from helpers import *
 import pandas as pd
+import math
 
 # traits: Clothes, Ears, Hat, Fur, Mouth, Eyes
 
@@ -72,3 +74,41 @@ def is_still_listed(ape, ape_data):
         return False
     else:
         return True
+
+    # TODO add logic if exchanged to another wallet
+
+
+def split_apes_by_num_traits():
+    """
+    Function splits ape data into a dict with 6 dataframes each with number of traits
+    Used once, data will never change
+    """
+    # ape_data["all_apes"] <-- this is what we could use?
+    all_apes = pd.read_csv(".\\csvs\\all_the_apes.csv")
+    print(all_apes.head())
+    apes_by_num_traits = {}
+    for i in range(1, 7):
+        apes_by_num_traits[f"{i}T"] = pd.DataFrame()
+
+    # TODO there is probably a way to do this in pandas which is way better
+    # altho this data will never actually change, so who cares?
+    for index, row in all_apes.iterrows():
+        no_traits = 0
+        for i in range(2,8):
+            if type(row[i]) == str:
+                no_traits += 1
+        apes_by_num_traits[f"{no_traits}T"] = apes_by_num_traits[f"{no_traits}T"].append(row)
+
+    for key, value in apes_by_num_traits.items():
+        value.to_csv(f".\csvs\\apes_by_trait_count\\{key}_apes.csv")
+
+    return apes_by_num_traits
+
+def get_rank_for_number_of_traits(apes):
+    """
+    This function ranks each ape for the number of traits they have vs the rariry of their traits
+    returns an object containing:
+    ape_id
+     
+    """
+    pass
