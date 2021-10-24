5545a3164abba6383c654d212d0009dcc706be08
Sam Pink <samuel.pink@outlook.com>
2021-10-24 18:20:17 +0100

    updated transfer events

--- os_data_update/store_transfer.py
+++ os_data_update/store_transfer.py
@@ -6,18 +6,18 @@ from helpers import *
 
 def update_transfer(epoc_last_updated):
     path = "csvs/all_transfers.csv"
-    # all_sales = pd.read_csv(path)
+    all_transfers = pd.read_csv(path)
 
-    all_transfers = pd.DataFrame()
+    #all_transfers = pd.DataFrame()
 
     for i in range(0, 50000):
         print(i)
-        events = get_transfers(i)
+        events = get_transfers(i, epoc_last_updated)
 
         try:
             assets_events = events["asset_events"]
         except:
-            print("d")
+            assets_events = []
 
         if len(assets_events) == 0:
             break
@@ -25,9 +25,9 @@ def update_transfer(epoc_last_updated):
         for a in assets_events:
             df = parse_ape_transfer(a)
             if df.empty == False:
-                # if all_sales[all_sales.sale_id == df.sale_id.item()].empty:
+                if all_transfers[all_transfers.transfer_event_id == df.transfer_event_id.item()].empty:
                 # only apped if id is not already stored
-                all_transfers = all_transfers.append(df)
+                    all_transfers = all_transfers.append(df)
 
     try:
         all_transfers.to_csv(path)
